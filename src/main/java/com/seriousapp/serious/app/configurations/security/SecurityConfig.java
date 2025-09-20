43c655cba5a91224c680f681c557517f9ec8149a
tmampa <tmampa94@gmail.com>
2025-09-20 10:45:21 +0200

    expose all endpoints

--- src/main/java/com/seriousapp/serious/app/configurations/security/SecurityConfig.java
+++ src/main/java/com/seriousapp/serious/app/configurations/security/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/auth/**", "/api/**").permitAll()
                         .requestMatchers("/api/admin/**").hasRole("ADMIN")
                         .requestMatchers("/api/student/**").hasRole("STUDENT")
                         .anyRequest().authenticated()
